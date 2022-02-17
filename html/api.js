a34b66cc0172293d04272855e12947060db2ff85
joerush18 <joras.aryal23@gmail.com>
2022-02-17 23:12:41 +0545

    small fix

--- html/api.js
+++ html/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://calorieninjas.p.rapidapi.com/v1/nutrition",
   headers: {
-    "x-rapidapi-key": process.env.RAPID_KEY,
-    "x-rapidapi-hos": process.env.RAPID_HOST_NAME,
+    "x-rapidapi-key": "da4b0cce76msh013da5a6a55baeap12bbd9jsn293f76613d94",
+    "x-rapidapi-hos": "calorieninjas.p.rapidapi.com",
   },
 });
 
